928a61f7f0bcebd8722bee78d19f1545e4886f94
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-16 16:52:26 -0700

    Constructor, printer

--- Grid.cpp
+++ Grid.cpp
@@ -1,3 +1,38 @@
 #include "Grid.h"
+#include <vector>
 
+using namespace std;
+
+Grid::Grid(int ni, int nj,
+	double xMin, double xMax, double yMin, double yMax)
+:ni(ni), nj(nj)
+,xMin(xMin), xMax(xMax), yMin(yMin), yMax(yMax)
+,xRange(xMax - xMin), yRange(yMax - yMin)
+,dx(xRange/nj), dy(yRange/ni)
+,x(ni, nj), y(ni, nj)
+{
+	vector<double> xx(nj), yy(ni);
+	for(int i=0; i<ni; i++)
+		yy[i] = yMax - (i + 0.5)*dy;
+	for(int j=0; j<nj; j++)
+		xx[j] = xMin + (j + 0.5)*dx;
+
+	// Fill the grid
+	for(int i=0; i<ni; i++)
+	{
+		for(int j=0; j<nj; j++)
+		{
+			x(i, j) = xx[j];
+			y(i, j) = yy[i];
+		}
+	}
+}
+
+
+void Grid::print(ostream& out) const
+{
+	for(int i=0; i<ni; i++)
+		for(int j=0; j<nj; j++)
+			out<<x(i, j)<<' '<<y(i, j)<<endl;
+}
 
--- Grid.h
+++ Grid.h
@@ -1,12 +1,24 @@
 #ifndef _Grid_
 #define _Grid_
 
+#include <ostream>
 #include "Array.h"
 
 class Grid
 {
 	private:
-		
+		// Borders, number of points, etc
+		int ni, nj;
+		double xMin, xMax, yMin, yMax;
+		double xRange, yRange, dx, dy;
+
+		Array x, y;
+
+	public:
+		Grid(int ni, int nj,
+			double xMin, double xMax, double yMin, double yMax);
+
+		void print(std::ostream& out) const;
 
 };
 
