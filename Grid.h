99cf53d6385acb5671fb5060d88606ffbb985ecf
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-16 21:23:38 -0700

    More getters

--- Grid.h
+++ Grid.h
@@ -19,8 +19,11 @@ class Grid
 			double xMin, double xMax, double yMin, double yMax);
 		void print(std::ostream& out) const;
 
-		// Read-only getter
+		// Read-only getters
 		double get_x(int i, int j) const { return x(i, j); }
+		double get_y(int i, int j) const { return y(i, j); }
+		int get_ni() const { return ni; }
+		int get_nj() const { return nj; }
 };
 
 #endif
