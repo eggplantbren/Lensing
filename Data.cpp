bd84d125d2e55948c8758f6f0426a3924dc8c2ad
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-16 22:08:28 -0700

    Some kind of loading happening

--- Data.cpp
+++ Data.cpp
@@ -1,5 +1,7 @@
 #include "Data.h"
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,6 +14,20 @@ Data::Data()
 void Data::load(const char* filename)
 {
 	fstream fin(filename, ios::in);
+
+	// Check that the file was opened successfully
+	if(!fin)
+	{
+		cerr<<"# Error. Couldn't open file "<<filename<<
+			" for reading. No data was loaded."<<endl;
+		fin.close();
+		return;
+	}
+
+	double temp;
+	vector<double> vec;
+	while(fin>>temp)
+		vec.push_back(temp);
 	fin.close();
 }
 
