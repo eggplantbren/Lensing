f8131bcca4292be0bf4db885cf5152489db601f3
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-20 17:16:10 -0700

    More getters

--- Image.h
+++ Image.h
@@ -24,8 +24,14 @@ class Image
 		double get_pixel(int i, int j) const { return pixels(i, j); }
 		int get_ni() const { return ni; }
 		int get_nj() const { return nj; }
-
-
+		double get_xMin() const { return xMin; }
+		double get_xMax() const { return xMax; }
+		double get_yMin() const { return yMin; }
+		double get_yMax() const { return yMax; }
+		double get_xRange() const { return xRange; }
+		double get_yRange() const { return yRange; }
+		double get_dx() const { return dx; }
+		double get_dy() const { return dy; }
 };
 
 #endif
