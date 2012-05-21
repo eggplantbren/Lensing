f6c0310e008d9327ca15a7739ac6693f69f5b9cf
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-20 17:01:02 -0700

    Skeleton of implementation, uniform latent parameters

--- Sources/Blobby.cpp
+++ Sources/Blobby.cpp
@@ -1,2 +1,27 @@
 #include "Blobby.h"
 
+using namespace std;
+
+const int Blobby::maxNumBlobs = 100;
+
+void Blobby::fromPrior()
+{
+
+}
+
+double Blobby::evaluate(double x, double y) const
+{
+	x = x + y;
+	return 0.;
+}
+
+double Blobby::perturb()
+{
+	return 0.;
+}
+
+void Blobby::print(ostream& out) const
+{
+	out<<' ';
+}
+
--- Sources/Blobby.h
+++ Sources/Blobby.h
@@ -6,18 +6,24 @@
 */
 
 #include "Profile.h"
+#include <vector>
 
 class Blobby:public Profile
 {
 	private:
+		// Maximum number of nonzero-mass blobs
+		static const int maxNumBlobs;
 
+		// Uniform latent variables
+		// (position, position, size, mass)
+		std::vector<double> u1, u2, u3, u4;
 
 	public:
 		// Generate from prior
 		void fromPrior();
 
 		// Surface brightness profile
-		double evaluate(double x, double y);
+		double evaluate(double x, double y) const;
 
 		// M-H proposals
 		double perturb();
